2579b104d1adfbf182615405c9bef85843c5f6d4
Abdou_Mourid <abdoumouhid96@yahoo.com>
2025-09-22 19:00:44 +0200

    Working with groups and permissions: Restricting access to class-based views

--- courses/views.py
+++ courses/views.py
@@ -1,7 +1,12 @@
+from django.contrib.auth.mixins import (
+    LoginRequiredMixin,
+    PermissionRequiredMixin,
+)
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.views.generic.list import ListView
+
 from .models import Course
 
 # Create your views here.
@@ -19,7 +24,9 @@ class OwnerEditMixin:
         return super().form_valid(form)
 
 
-class OwnerCourseMixin(OwnerMixin):
+class OwnerCourseMixin(
+    OwnerMixin, LoginRequiredMixin, PermissionRequiredMixin
+):
     model = Course
     fields = ["subject", "title", "slug", "overview"]
     success_url = reverse_lazy("manage_course_list")
@@ -31,15 +38,17 @@ class OwnerCourseEditMixin(OwnerCourseMixin, OwnerEditMixin):
 
 class ManageCourseListView(OwnerCourseMixin, ListView):
     template_name = "courses/manage/course/list.html"
+    permission_required = "courses.view_course"
 
 
 class CourseCreateView(OwnerCourseEditMixin, CreateView):
-    pass
+    permission_required = "courses.add_course"
 
 
 class CourseUpdateView(OwnerCourseEditMixin, UpdateView):
-    pass
+    permission_required = "courses.change_course"
 
 
 class CourseDeleteView(OwnerCourseMixin, DeleteView):
     template_name = "courses/manage/course/delete.html"
+    permission_required = "courses.delete_course"
