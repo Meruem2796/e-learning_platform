b64bc2e52ae0cfe4f2ab84f3acc3a726f0ba4ada
Abdou_Mourid <abdoumouhid96@yahoo.com>
2025-12-25 02:18:43 +0100

    Using Redis cache backend: Monitoring Redis with Django Redisboard

--- educa/settings.py
+++ educa/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     "students.apps.StudentsConfig",
     "embed_video",
     "debug_toolbar",
+    "redisboard",
 ]
 
 MIDDLEWARE = [
@@ -145,8 +146,8 @@ SECURE_REFERRER_POLICY = "strict-origin-when-cross-origin"
 
 CACHES = {
     "default": {
-        "BACKEND": "django.core.cache.backends.memcached.PyMemcacheCache",
-        "LOCATION": "127.0.0.1:11211",
+        "BACKEND": "django.core.cache.backends.redis.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379",
     }
 }
 
