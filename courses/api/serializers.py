14d03b016da2411d9110a376cc03cbdcca5f2727
Abdou_Mourid <abdoumouhid96@yahoo.com>
2025-12-30 20:29:35 +0100

    Adding pagination to views

--- courses/api/serializers.py
+++ courses/api/serializers.py
@@ -1,5 +1,6 @@
 from django.db.models import Count
 from rest_framework import serializers
+
 from courses.models import Subject
 
 
@@ -10,7 +11,7 @@ class SubjectSerializer(serializers.ModelSerializer):
     def get_popular_courses(self, obj):
         courses = obj.courses.annotate(
             total_students=Count("students")
-        ).order_by("total_students")[:3]
+        ).order_by("-total_students")[:3]
         return [f"{c.title} ({c.total_students})" for c in courses]
 
     class Meta:
--- courses/api/views.py
+++ courses/api/views.py
@@ -1,12 +1,14 @@
 from django.db.models import Count
 from rest_framework import generics
-from courses.api.serializers import SubjectSerializer
 from courses.models import Subject
+from courses.api.pagination import StandardPagination
+from courses.api.serializers import SubjectSerializer
 
 
 class SubjectListView(generics.ListAPIView):
     queryset = Subject.objects.annotate(total_courses=Count("courses"))
     serializer_class = SubjectSerializer
+    pagination_class = StandardPagination
 
 
 class SubjectDetailView(generics.RetrieveAPIView):
