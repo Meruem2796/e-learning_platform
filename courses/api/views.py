34cbfa072f319bb2ce8a7b446163f4ef206b4a40
Abdou_Mourid <abdoumouhid96@yahoo.com>
2026-01-03 12:18:57 +0100

    Adding permissions to viewsatio

--- courses/api/views.py
+++ courses/api/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import get_object_or_404
 from rest_framework import generics
 from rest_framework import viewsets
 from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -36,6 +37,7 @@ class CourseViewSet(viewsets.ReadOnlyModelViewSet):
 
 class CourseEnrollView(APIView):
     authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
 
     def post(self, request, pk, format=None):
         course = get_object_or_404(Course, pk=pk)
