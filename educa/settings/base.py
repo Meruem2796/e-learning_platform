08af049c28236b7c9ee9a507af2f41c81143de5a
Abdou_Mourid <abdoumouhid96@yahoo.com>
2026-07-24 02:17:12 +0200

    Local environment settings

--- educa/settings/base.py
+++ educa/settings/base.py
@@ -27,7 +27,6 @@ SECRET_KEY = (
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -87,13 +86,6 @@ WSGI_APPLICATION = "educa.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
 
 
 # Password validation
